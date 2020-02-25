914602c461997128724198b9c9618b1a445b4d0e
Armin93 <amushik93@mail.ru>
2020-02-25 13:24:06 +0400

    minor changes

--- sql/db.sql
+++ sql/db.sql
@@ -11,8 +11,8 @@ CREATE TABLE IF NOT EXISTS users(
   email VARCHAR(100) NOT NULL UNIQUE,
   password VARCHAR(100) NOT NULL
 );
-insert into notes(note_uid,note) values(uuid_generate_v4(),'hihello');
-insert into users(user_uid,email,password) values(uuid_generate_v4(),'arminmheryan@gmail.com',crypt('12345', gen_salt('bf', 8)));
+insert into notes(note_uid,note) values(uuid_generate_v4(),'<note>');
+insert into users(user_uid,email,password) values(uuid_generate_v4(),'<mail>',crypt('<password>', gen_salt('bf', 8)));
  
  
 CREATE TRIGGER set_timestamp
